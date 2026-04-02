6c0ece10d698eab8d612321a80f8ea8b01cf9ddf
Rafael Viana <r.viana@iesb.edu.br>
2026-04-02 18:58:27 -0300

    feat: conclui pratica02. Fecha #ID

---
+++ praticas/pratica02/imprima_decimais.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main() {
+
+    double pi = 3.1415926;
+    
+    printf("%.7f\n", pi);
+    printf("%.2f\n", pi);
+    
+    return 0;
+}
---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("10\n");
+    printf("-5\n");
+    printf("00004\n");
+    
+    return 0;
+}
