33b0348c8b1b799adbd660613512e0e0f9315cb9
Rafael Viana <r.viana@iesb.edu.br>
2026-04-02 19:19:45 -0300

    pratica 02. versao 2.0 (inclusao de variaveis)

--- praticas/pratica02/imprima_formatado.c
+++ praticas/pratica02/imprima_formatado.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
 int main() {
-    
-    printf("==============================\n");
+
+    char Produto1[] = "Camiseta";
+    char Produto2[] = "Calca";
+    char Produto3[] = "Meia Social";
+
+    int quantidadeProduto1 = 2;
+    int quantidadeProduto2 = 1;
+    int quantidadeProduto3 = 3;
+
+    float valorUnitarioProduto1 = 39.99;
+    float valorUnitarioProduto2 = 89.90;
+    float valorUnitarioProduto3 = 19.99;
+
+    float valorTotal = (quantidadeProduto1 * valorUnitarioProduto1) + 
+                       (quantidadeProduto2 * valorUnitarioProduto2) + 
+                       (quantidadeProduto3 * valorUnitarioProduto3);
+
+printf("==============================\n");
     printf("      N O T A    L E G A L\n");
     printf("==============================\n");
-    printf("Produto         Qtd Valor Unit\n");
-    printf("Camiseta        002      39.99\n");
-    printf("Calca           001      89.90\n");
-    printf("Meia Social     003      19.99\n");
-    printf("==============================\n");
-    printf("Total                   229.85\n");
 
+    printf("%-15s %s %10s\n", "Produto", "Qtd", "Valor Unit");
+
+    printf("%-15s %03d %10.2f\n", Produto1, quantidadeProduto1, valorUnitarioProduto1);
+    printf("%-15s %03d %10.2f\n", Produto2, quantidadeProduto2, valorUnitarioProduto2);
+    printf("%-15s %03d %10.2f\n", Produto3, quantidadeProduto3, valorUnitarioProduto3);
+
+    printf("==============================\n");
+
+    printf("%-15s %14.2f\n", "Total", valorTotal);
     return 0;
 }
